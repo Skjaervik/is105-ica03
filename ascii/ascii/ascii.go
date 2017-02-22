8a0f6e8b17d60707e6f739b2f66a3d83ff40ce27
Mats Skjaervik <matses15@student.uia.no>
2017-02-22 16:25:52 +0100

    oppdater

--- ascii/ascii/ascii.go
+++ ascii/ascii/ascii.go
@@ -1,6 +1,8 @@
 package ascii
 
-const ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
+import "fmt"
+
+const Ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
 	"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f" +
 	` !"#$%&'()*+,-./0123456789:;<=>?` +
 	`@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_` +
@@ -18,6 +20,9 @@ const ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
 // ...
 func IterateOverASCIIStringLiteral(sl string) {
 	// Kode for Oppgave 1a
+	for i := 0; i < len(sl); i++ {
+		fmt.Printf("%X %+q %b \n", sl[i], sl[i], sl[i])
+	}
 }
 
 // Funksjonen skal generere en utskrift fra en sekvens av bytes,
@@ -28,4 +33,19 @@ func IterateOverASCIIStringLiteral(sl string) {
 // Funksjonen greetingASCII() returnerer en variabel av typen string,
 // som inneholder kun ASCII tegn (ikke utvidet ASCII).
 // Gjelder oppgave 1b
-func GreetingASCII() {}
+/*func GreetingASCII() {
+	const s = "48656C6C6F203B2D29"
+	decoded, err := hex.DecodeString(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", decoded)
+}
+*/
+
+func GreetingASCII2() {
+	a := "\x22\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29\x22"
+	for i := 0; i < len(a); i++ {
+		fmt.Printf("%c", a[i])
+	}
+}
---
+++ ascii/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "./ascii"
+
+func main() {
+	//ascii.IterateOverASCIIStringLiteral(ascii.Ascii)
+	//ascii.GreetingASCII()
+	ascii.GreetingASCII2()
+}
